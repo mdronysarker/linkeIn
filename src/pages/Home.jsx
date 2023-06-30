d76c4478881d661042a7e61e3515999988521d2d
Md Rony <mdronyrsk01724@gmail.com>
2023-06-30 21:42:58 +0600

    home nav design completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,27 @@ const Home = () => {
   return (
     <>
       <Header />
+      <Container>
+        <Section>
+          <h5>
+            <a>Hiring in a hurry ?</a>
+            <p>
+              Find talented pros in record time with Upwork and keep busness
+              moving.
+            </p>
+          </h5>
+        </Section>
+      </Container>
     </>
   );
 };
+
+const Container = styled.div`
+  padding-top: 52px;
+`;
+
+const Conttent = styled.div``;
+
+const Section = styled.section``;
 
 export default Home;
